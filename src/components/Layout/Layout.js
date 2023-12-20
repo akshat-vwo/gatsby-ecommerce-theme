b50cec43f6f40f98fb80441d86c18baf367e2db5
akshat-vwo <153659356+akshat-vwo@users.noreply.github.com>
2023-12-20 15:29:06 +0530

    minor fix

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -28,7 +28,9 @@ const Layout = ({ props, children, disablePaddingBottom = false }) => {
         <link rel="preconnect" href="https://dev.visualwebsiteoptimizer.com" />
 
         <script>
-          {(function (w, d, s, l, i) {
+          {
+          typeof window !== undefined ? 
+          (function (w, d, s, l, i) {
             w[l] = w[l] || []; w[l].push({
               'gtm.start':
                 new Date().getTime(), event: 'gtm.js'
@@ -36,7 +38,7 @@ const Layout = ({ props, children, disablePaddingBottom = false }) => {
               j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
                 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
           })(window, document, 'script', 'dataLayer', 'GTM-WMFD6FWH')
-          }</script>
+          : null }</script>
         <script type='text/javascript' id='vwoCode'>
           {`
               // window._vwo_code || (function () {
